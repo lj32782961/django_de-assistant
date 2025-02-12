3d90e9b9a13ae432d22a7f2d90828be269c45e0c
lj32782961 <lj32782961@gmail.com>
2025-02-13 00:06:21 +0100

    remove /admin path

--- webapp/urls.py
+++ webapp/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 #开发环境下
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', include('aichat.urls')),
 ]#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
